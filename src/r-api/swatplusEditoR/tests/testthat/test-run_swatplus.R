94d7c6c124091a00c601d47f3ccd8af067cdd78e
tadhg-moore <tadhg.moore6@gmail.com>
2026-03-31 14:36:52 +1300

    Upate test

--- src/r-api/swatplusEditoR/tests/testthat/test-run_swatplus.R
+++ src/r-api/swatplusEditoR/tests/testthat/test-run_swatplus.R
@@ -22,32 +22,21 @@ setup_swat_sim <- function(project_dir) {
     quiet = TRUE
   ) 
   init_gwflow(project, cell_size = 200, row_count = 50, col_count = 60)
-  stations <- data.frame(
-    name = c("station1", "station2"),
-    lat = c(-38.1, -38.2),
-    lon = c(176.3, 176.4),
-    pcp = c("pcp1.cli", "pcp2.cli"),
-    tmp = c("tmp1.cli", "tmp2.cli"),
-    slr = c("slr1.cli", "slr2.cli"),
-    hmd = c("hmd1.cli", "hmd2.cli"),
-    wnd = c("wnd1.cli", "wnd2.cli"),
-    pet = c("pet1.cli", "pet2.cli"),
-    atmo_dep = c("atmo1.cli", "atmo2.cli"),
-    stringsAsFactors = FALSE
-  )
   set_simulation_time(project, day_start = 1, yrc_start = 2000,
                       day_end = 365, yrc_end = 2010)
-  add_weather_stations(project, stations)
 }
 
 test_that("run SWAT+ with basic files", {
 
-  output_dir <- tempfile("txtinout_")
+  # output_dir <- tempfile("txtinout_")
   
   project_dir <- "swat_test"
   project <- setup_swat_sim(project_dir)
-  
-  write_config_files(project, output_dir = project_dir)
+  # Set weather dir to ERA5 data from package
+  weather_dir <- system.file("extdata", "era5", package = "swatplusEditoR")
+
+  write_config_files(project, output_dir = project_dir,
+                     weather_dir = weather_dir)
   
   run_swatplus(working_dir = output_dir)
 })
